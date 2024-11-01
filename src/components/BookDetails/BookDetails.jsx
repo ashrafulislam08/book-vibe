013797aee7f4375c62aa2df7f2caeda5a30f26c7
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-01 22:54:57 +0600

    Added full design of book details components

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -6,20 +6,68 @@ const BookDetails = () => {
   const data = useLoaderData();
   const id = parseInt(bookId);
   const book = data.find((book) => book.bookId === id);
-  const { bookId: currentBookId, image, bookName } = book;
+  const {
+    bookId: currentBookId,
+    image,
+    bookName,
+    author,
+    category,
+    review,
+    tags,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+  } = book;
 
   return (
     <div className="my-12 bg-base-200 ">
-      <div className="hero  min-h-screen max-w-4xl mx-auto">
+      <div className="hero  min-h-screen max-w-5xl mx-auto">
         <div className="hero-content flex-col lg:flex-row">
-          <img src={image} className="max-w-52 rounded-lg shadow-2xl" />
+          <img src={image} className="max-w-[50%] rounded-lg shadow-2xl" />
           <div className="text-left">
             <h1 className="text-4xl font-bold">{bookName}</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <p>By {author}</p>
+            <div className="divider my-1"></div>
+            <p>{category}</p>
+            <div className="divider my-1"></div>
+            <p className="py-2 mb-5">
+              <span className="font-bold">Review:</span> {review}
             </p>
+
+            <div className="flex my-5 gap-4">
+              <span className="font-bold text-gray-500">Tags:</span>
+              {tags.map((tag, idx) => (
+                <button
+                  key={idx}
+                  className="btn btn-xs bg-green-200 text-green-500"
+                >
+                  {tag}
+                </button>
+              ))}
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="m-0">
+              <div className="flex gap-6 my-2 text-gray-500">
+                <p>Number of pages: </p>
+                <p className="text-black">{totalPages}</p>
+              </div>
+              <div className="flex gap-6 my-2 text-gray-500">
+                <p>Publisher: </p>
+                <p className="text-black">{publisher}</p>
+              </div>
+              <div className="flex gap-6 my-2 text-gray-500">
+                <p>Year of Publishing: </p>
+                <p className="text-black">{yearOfPublishing}</p>
+              </div>
+              <div className="flex gap-6 my-2 text-gray-500">
+                <p>Rating: </p>
+                <p className="text-black">{rating}</p>
+              </div>
+            </div>
+
             <div>
               <button className="btn btn-outline btn-accent mr-4">Read</button>
               <button className="btn btn-accent">Wishlist</button>
